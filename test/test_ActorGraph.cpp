171e9120224c036452b83ed3210bb35519269510
Aimee Shao <aimeeshao101@gmail.com>
2019-11-28 03:09:27 +0000

    Fixed testing files

--- test/test_ActorGraph.cpp
+++ test/test_ActorGraph.cpp
@@ -13,27 +13,28 @@ using namespace testing;
 TEST(ActorGraphTests, SIMPLE_TEST) {
     ActorGraph graph;
     // Assert loading file works
-    ASSERT_EQ(graph.loadFromFile("../data/imdb_small_sample.tsv", true), true);
+    ASSERT_EQ(graph.loadFromFile("../test/data/imdb_small_sample.tsv", true),
+              true);
 }
 
 TEST(ActorGraphTests, INVALID_FILE_TEST) {
     ActorGraph graph;
     // Assert loading invalid file returns false
-    ASSERT_EQ(graph.loadFromFile("invalid.tsv", false), false);
+    ASSERT_EQ(graph.loadFromFile("../test/data/invalid.tsv", false), false);
 }
 
 TEST(ActorGraphTests, PATHFIND_TEST) {
     ActorGraph graph;
-    graph.loadFromFile("../data/imdb_small_sample.tsv", false);
-    const char* pairsFileName = "../data/test_small_pairs.tsv";
+    graph.loadFromFile("../test/data/imdb_small_sample.tsv", false);
+    const char* pairsFileName = "../test/data/test_small_pairs.tsv";
     // Assert path finding works
     ASSERT_EQ(graph.pathFind(pairsFileName, cout), true);
 }
 
 TEST(ActorGraphTests, LINKPREDICT_TEST) {
     ActorGraph graph;
-    graph.loadFromFile("../data/imdb_small_sample.tsv", false);
-    const char* actorsFileName = "../data/test_actors.tsv";
+    graph.loadFromFile("../test/data/imdb_small_sample.tsv", false);
+    const char* actorsFileName = "../test/data/test_actors.tsv";
     // Assert linkpredictor works
     ASSERT_EQ(graph.linkPredict(actorsFileName, cout, cout), true);
 }
